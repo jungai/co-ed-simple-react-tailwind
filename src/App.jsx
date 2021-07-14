ac1cde6033b09d588838984de417d5481c0cbfa5
jungai <juworaphol125@gmail.com>
2021-07-14 17:59:28 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="h-screen grid place-content-center">
+      <h1 className="text-2xl text-red-300 underline">Hello</h1>
+    </div>
+  )
+}
+
+export default App
